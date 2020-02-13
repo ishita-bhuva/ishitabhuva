3ea9d203523c7a07fa31c74d21347a478226fda0
ishita <ishita@logisticinfotech.co.in>
2020-02-13 13:03:44 +0530

    loader issue solve

--- src/js/script.js
+++ src/js/script.js
@@ -54,16 +54,10 @@
 // });
 
 function hideLoader() {
-    $('.jh-loader').fadeOut(1000);
+    $('.ib-loader').fadeOut(600);
 }
 
 function init() {
     hideLoader();
-    var vidDefer = document.getElementsByTagName('iframe');
-    for (var i = 0; i < vidDefer.length; i++) {
-        if (vidDefer[i].getAttribute('data-src')) {
-            vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
-        }
-    }
 }
 window.onload = init;
