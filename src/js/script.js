1b179edf22e148354de51bc7ba5691827a2b7eab
ishita <ishita@logisticinfotech.co.in>
2020-02-13 12:18:26 +0530

    live build

--- src/js/script.js
+++ src/js/script.js
@@ -34,24 +34,24 @@
               
 })(jQuery);
 
-$(document).ready(function() {
-    $('.navbar-toggler').on('click', function(e) {
-        if ($(this).hasClass('ib-close')) {
-            $('body').addClass('ib-menu-close');
-            $('.ib-menu').addClass('ib-show');
-            setTimeout(() => {
-                $('body').removeClass('ib-menu-open');
-                $('body').removeClass('ib-menu-close');
-                $(this).removeClass('ib-close');
-            }, 1400);
-        } else {
-            $('body').addClass('ib-menu-open');
-            setTimeout(() => {
-                $(this).addClass('ib-close');
-            }, 1000);
-        }
-    });
-});
+// $(document).ready(function() {
+//     $('.navbar-toggler').on('click', function(e) {
+//         if ($(this).hasClass('ib-close')) {
+//             $('body').addClass('ib-menu-close');
+//             $('.ib-menu').addClass('ib-show');
+//             setTimeout(() => {
+//                 $('body').removeClass('ib-menu-open');
+//                 $('body').removeClass('ib-menu-close');
+//                 $(this).removeClass('ib-close');
+//             }, 1400);
+//         } else {
+//             $('body').addClass('ib-menu-open');
+//             setTimeout(() => {
+//                 $(this).addClass('ib-close');
+//             }, 1000);
+//         }
+//     });
+// });
 
 function hideLoader() {
     $('.jh-loader').fadeOut(1000);
